237e96f4dbd6284413555ddde6e8b6b3aace1874
tande1124 <tande1124@163.com>
2019-09-05 10:21:57 +0800

    delete

--- src/Tools/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @LastEditors: lyuwei
- * @Author: lyuwei
- * @Date: 2019-01-04 16:21:29
- * @LastEditTime: 2019-04-29 14:47:25
- */
-import * as CoordTrans from './CoordTrans'
-
-export { CoordTrans }
