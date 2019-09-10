74d982d7cf1ae93767669d6cf7d1ef76091d1c5c
tande1124 <tande1124@163.com>
2019-09-10 23:45:04 +0800

    update

--- src/SeeGate/index.js
+++ src/SeeGate/index.js
@@ -2,13 +2,7 @@
  * @Descripttion: 
  * @Date: 2019-09-05 11:18:43
  * @LastEditors: tande
- * @LastEditTime: 2019-09-05 13:06:42
+ * @LastEditTime: 2019-09-10 23:44:41
  */
-export {
-  default as SeeGate
-}
-from './SeeGate'
-export {
-  default as SeeGateEventType
-}
-from './SeeGateEventType'
+export { default as SeeGate } from './SeeGate'
+export { default as SeeGateEventType } from './SeeGateEventType'
