a67673f4282c587ba8e62ef1cec0468e33b600bf
tande <tande@gsatefy.com>
2019-11-20 17:07:52 +0800

    在线地图

--- example/src/components/gis/mapBase.vue
+++ example/src/components/gis/mapBase.vue
@@ -1,11 +1,11 @@
 <!--
- * @LastEditors: lyuwei
+ * @LastEditors: tande
  * @Author: lyuwei
  * @Date: 2019-02-21 11:44:42
- * @LastEditTime: 2019-05-24 17:40:24
+ * @LastEditTime: 2019-11-20 16:14:04
  -->
 <template>
-  <div :id="mapdivname"></div>
+	<div :id="mapdivname"></div>
 </template>
 
 <script>
@@ -20,7 +20,7 @@ export default {
     }
   },
 
-  mounted: function () {
+  mounted () {
     this.thismap = new Map(this.mapdivname, {
       serviceUrl: 'http://localhost:8080',
       resourceUrl: process.env.BASE_URL + 'map_resource',
